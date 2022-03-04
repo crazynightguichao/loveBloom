da4b64571e3e48d4d32b8cadaf4871f2dc11d627
jiaguichao <47845006+crazynightguichao@users.noreply.github.com>
2022-03-04 15:45:57 +0800

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,13 @@
-var $window = $(window), gardenCtx, gardenCanvas, $garden, garden;
- var clientWidth = $(window).width();
-  var clientHeight = $(window).height(); 
+// var $window = $(window), gardenCtx, gardenCanvas, $garden, garden;
+//  var clientWidth = $(window).width();
+//   var clientHeight = $(window).height(); 
 //   $(function () { 
 //         $loveHeart = $("#loveHeart"); 
 //         var a = $loveHeart.width() / 2; 
 //         var b = $loveHeart.height() / 2 - 55; 
-//         $garden = $("#garden"); 
-//         gardenCanvas = $garden[0]; 
+//         // $garden = $("#garden"); 
+//         // gardenCanvas = $garden[0]; 
+//         gardenCanvas = $("#loveHeart"); 
 //         gardenCanvas.width = $("#loveHeart").width(); 
 //         gardenCanvas.height = $("#loveHeart").height(); 
 //         gardenCtx = gardenCanvas.getContext("2d");
@@ -19,42 +20,42 @@ var $window = $(window), gardenCtx, gardenCanvas, $garden, garden;
 //         setInterval(function () {
 //              garden.render() 
 //         }, Garden.options.growSpeed) }); 
-        $(window).resize(function () {
-            var b = $(window).width(); 
-            var a = $(window).height(); 
-            if (b != clientWidth && a != clientHeight) { 
-                location.replace(location) } 
-            }); 
-            function getHeartPoint(c) { 
-                var b = c / Math.PI; var a = 19.5 * (16 * Math.pow(Math.sin(b), 3)); 
-                var d = -20 * (13 * Math.cos(b) - 5 * Math.cos(2 * b) - 2 * Math.cos(3 * b) - Math.cos(4 * b)); 
-                return new Array(offsetX + a, offsetY + d) 
-            } 
-            function startHeartAnimation() { 
-                var c = 50; var d = 10; var b = new Array(); 
-                var a = setInterval(function () { 
-                    var h = getHeartPoint(d); var e = true; 
-                    for (var f = 0; f < b.length; f++) { 
-                        var g = b[f]; var j = Math.sqrt(Math.pow(g[0] - h[0], 2) + Math.pow(g[1] - h[1], 2)); 
-                        if (j < Garden.options.bloomRadius.max * 1.3) { 
-                            e = false; break } 
-                        } if (e) { 
-                            b.push(h); garden.createRandomBloom(h[0], h[1]) 
-                        } if (d >= 30) { 
-                            clearInterval(a); showMessages() 
-                        } else { d += 0.2 } }, c) 
-                    } (function (a) { a.fn.typewriter = function () { 
-                        this.each(function () { 
-                            var d = a(this), c = d.html(), b = 0; d.html(""); 
-                            var e = setInterval(function () { 
-                                var f = c.substr(b, 1); 
-                                if (f == "<") { 
-                                    b = c.indexOf(">", b) + 1 
-                                } else {
-                                     b++ 
-                                } d.html(c.substring(0, b) + (b & 1 ? "_" : "")); 
-                                if (b >= c.length) { clearInterval(e) } }, 75) }); 
-                                return this } })(jQuery); 
+//         $(window).resize(function () {
+//             var b = $(window).width(); 
+//             var a = $(window).height(); 
+//             if (b != clientWidth && a != clientHeight) { 
+//                 location.replace(location) } 
+//             }); 
+//             function getHeartPoint(c) { 
+//                 var b = c / Math.PI; var a = 19.5 * (16 * Math.pow(Math.sin(b), 3)); 
+//                 var d = -20 * (13 * Math.cos(b) - 5 * Math.cos(2 * b) - 2 * Math.cos(3 * b) - Math.cos(4 * b)); 
+//                 return new Array(offsetX + a, offsetY + d) 
+//             } 
+//             function startHeartAnimation() { 
+//                 var c = 50; var d = 10; var b = new Array(); 
+//                 var a = setInterval(function () { 
+//                     var h = getHeartPoint(d); var e = true; 
+//                     for (var f = 0; f < b.length; f++) { 
+//                         var g = b[f]; var j = Math.sqrt(Math.pow(g[0] - h[0], 2) + Math.pow(g[1] - h[1], 2)); 
+//                         if (j < Garden.options.bloomRadius.max * 1.3) { 
+//                             e = false; break } 
+//                         } if (e) { 
+//                             b.push(h); garden.createRandomBloom(h[0], h[1]) 
+//                         } if (d >= 30) { 
+//                             clearInterval(a); showMessages() 
+//                         } else { d += 0.2 } }, c) 
+//                     } (function (a) { a.fn.typewriter = function () { 
+//                         this.each(function () { 
+//                             var d = a(this), c = d.html(), b = 0; d.html(""); 
+//                             var e = setInterval(function () { 
+//                                 var f = c.substr(b, 1); 
+//                                 if (f == "<") { 
+//                                     b = c.indexOf(">", b) + 1 
+//                                 } else {
+//                                      b++ 
+//                                 } d.html(c.substring(0, b) + (b & 1 ? "_" : "")); 
+//                                 if (b >= c.length) { clearInterval(e) } }, 75) }); 
+//                                 return this } })(jQuery); 
                                 function timeElapse(c) { 
                                     var e = Date(); 
                                     var f = (Date.parse(e) - Date.parse(c)) / 1000; 
